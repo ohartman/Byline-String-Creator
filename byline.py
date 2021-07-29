10416d9ab7b217359e7b6f5f3988fefe425241f3
roh29 <roh29@il-roh29-m1.T-mobile.com>
2021-07-29 12:14:08 -0400

    byline

---
+++ byline.py
@@ -0,0 +1,83 @@
+"""
+Instructions for the exercise
+-   Create the function "create_byline_string" in Python per the requirements below
+-   Write a program using the function to demonstrate that it works
+-   Email a link to a github repository containing the program
+Requirements for create_byline_string
+-   The function must accept a valid object format
+-   The function must remove invalid objects
+-   The function must output a valid byline HTML string
+-   A byline string must start with a "By"
+-   Authors must be separated by a comma "," if there are more than 2
+-   The last Author must be separated by an "and"
+-   The Author must be wrapped by the style specified in the "block" parameter
+-   Assume that we support the `<em>` and `<strong>` html tags
+The example input below should return:
+   "By <strong>Jonah Engel Bromwich</strong>, <em>Matthew Schneier</em> and Niraj Chokshi"
+"""
+from typing import List
+
+example_bylines = {
+    'authors': [
+        {
+            'firstName': 'jonah',
+            'middleName': 'Engel',
+            'lastName': 'bromwich',
+            'block': {
+                '__typename': 'Bold'
+            }
+        },
+        {'random': 'node'},
+        {},
+        {
+            'firstName': 'matthew',
+            'middleName': '',
+            'lastName': 'sChneier',
+            'block': {
+                '__typename': 'Italics'
+            }
+        },
+        {
+            'firstName': 'Niraj',
+            'middleName': '',
+            'lastName': 'chokshi',
+            'block': {}
+        }
+    ]
+}
+
+def create_byline_string(bylines: dict[str, List]) -> str:
+    # TODO
+    output = 'By '
+    conjunction = ', '
+    while len(bylines['authors']) != 0:
+        if len(bylines['authors'])==1:
+            conjunction = ''
+            output = output + 'and'
+        line = bylines['authors'].pop(0)
+        if 'firstName' in line.keys():
+            if line['middleName'] != '':
+                if line.get('block', {}).get('__typename') == 'Bold':
+                    output  = output + '<strong>' + line['firstName'].capitalize() + '/strong> ' + line['middleName'].capitalize() + ' ' + line['lastName'].capitalize() + conjunction
+                elif line.get('block', {}).get('__typename') == 'Italics':
+                    output  = output + '<em>' + line['firstName'].capitalize() + '/em> ' + line['middleName'].capitalize() + ' ' + line['lastName'].capitalize() + conjunction
+                else:
+                    output  = output + ' ' + line['firstName'].capitalize() + ' ' + line['middleName'].capitalize() + ' ' + line['lastName'].capitalize() + conjunction
+            else:
+                if line.get('block', {}).get('__typename') == 'Bold':
+                    output  = output + '<strong>' + line['firstName'].capitalize() + '/strong> ' + line['lastName'].capitalize() + conjunction
+                elif line.get('block', {}).get('__typename') == 'Italics':
+                    output  = output + '<em>' + line['firstName'].capitalize() + '/em> ' + line['lastName'].capitalize() + conjunction
+                else:
+                    output  = output + ' ' + line['firstName'].capitalize() + ' ' + line['lastName'].capitalize() + conjunction
+    return output
+                        
+
+
+
+
+
+
+                    
+
+print(create_byline_string(example_bylines))
